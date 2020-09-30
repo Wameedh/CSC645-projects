c534f3fe0cdf963dd273695b0c2ee44dabfccb90
Wameedh Mohammed Ali <wameedh.wf@gmail.com>
2020-09-30 15:17:00 -0700

    Lab4 completed!

--- Labs/lab4/client_handler.py
+++ Labs/lab4/client_handler.py
@@ -1,11 +1,11 @@
 ########################################################################################################################
 # Class: Computer Networks
-# Date: 02/03/2020
+# Date: 09/29/2020
 # Lab3: Server support for multiple clients
 # Goal: Learning Networking in Python with TCP sockets
-# Student Name:
-# Student ID:
-# Student Github Username:
+# Student Name: Wameedh Mohammed Ali
+# Student ID: 920678405
+# Student Github Username: wameedh
 # Lab Instructions: No partial credit will be given. Labs must be completed in class, and must be committed to your
 #               personal repository by 9:45 pm.
 # Running instructions: This program needs the server to run. The server creates an object of this class.
@@ -43,11 +43,22 @@ class ClientHandler:
         TODO: keep this handler object listening for more incoming data from the client
         :return: VOID
         """
-        pass # remove this line after implemented.
+        while True:
+            data = self.receive()  # receive data from client
+            if not data:  # if no data received end loop
+                break
+            student_name = data['student_name']  # get student name
+            github_username = data['github_username']  # get github username
+            sid = data['sid']  # get student id
+            log = "Connected: Student: " + student_name + \
+                  ", Github Username: " + github_username + ", sid: " + str(sid)
+            self.print_lock.acquire() # lock adquired only one client can write in memory allocation
+            print(log)  # print log
+            self.print_lock.release() # lock is released, other clients now can adquire it to write in the same memory allocation.
 
     def send(self, data):
         serialized_data = pickle.dumps(data)
-        self.handler.send(data)
+        self.handler.send(serialized_data)
 
     def receive(self, max_mem_alloc=4096):
         raw_data = self.handler.recv(max_mem_alloc)
